2760e135a334cd5fc762576afdd54c5206ca37eb
yogeshk05 <108150135+YogeshK05@users.noreply.github.com>
2025-05-12 18:39:44 +0000

    App complete.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 2000,
+    expectedReturn: 5,
+    duration: 10,
+  });
+
+  const isValidDuration = userInput.duration > 1;
+
+  function handleInputChange(selectedInput, changedValue) {
+    setUserInput((prevState) => ({
+      ...prevState,
+      [selectedInput]: +changedValue,
+    }));
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleInputChange} />
+      {!isValidDuration && (
+        <p className="center">Please enter duration greater than zero.</p>
+      )}
+      {isValidDuration && <Results userInput={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
